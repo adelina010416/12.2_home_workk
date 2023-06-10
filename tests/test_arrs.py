01aca165611ec0f788e40e501fa448cc0d2d3b3e
adelina010416 <adelinazima@mail.ru>
2023-06-10 19:28:00 +0400

    added fixtures

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,16 +3,19 @@ import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def coll():
+    return [1, 2, 3, 4]
+
+
+def test_get(coll):
+    assert arrs.get(coll, 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(coll):
+    assert arrs.my_slice(coll, 1, 3) == [2, 3]
+    assert arrs.my_slice(coll, 1) == [2, 3, 4]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3, 4], -3, 1) == []
-    assert arrs.my_slice([1, 2, 3, 4], -5, 4) == [1, 2, 3, 4]
-
-
+    assert arrs.my_slice(coll, -3, 1) == []
+    assert arrs.my_slice(coll, -5, 4) == coll
